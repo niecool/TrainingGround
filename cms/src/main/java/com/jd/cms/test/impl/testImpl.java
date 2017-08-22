99e0fc988447fe76d3cb97316d39a8e42470b5ce
zhaochengye <zhaochengye@jd.com>
2017-08-22 17:29:15 +0800

    test

---
+++ cms/src/main/java/com/jd/cms/test/impl/testImpl.java
@@ -0,0 +1,7 @@
+package com.jd.cms.test.impl;
+
+import com.jd.cms.test.test;
+
+public class testImpl implements test {
+
+}
---
+++ cms/src/main/java/com/jd/cms/test/test.java
@@ -0,0 +1,5 @@
+package com.jd.cms.test;
+
+public interface test {
+
+}
