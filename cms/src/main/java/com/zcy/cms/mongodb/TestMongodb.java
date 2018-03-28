6a0737145b5368eea6b9ca4fe1a33973cbea7a54
zhaochengye <zhaochengye@jd.com>
2018-03-28 16:52:00 +0800

    mongodbDemo

---
+++ cms/src/main/java/com/zcy/cms/mongodb/TestMongodb.java
@@ -0,0 +1,40 @@
+package com.zcy.cms.mongodb;
+
+import com.mongodb.MongoClient;
+import com.mongodb.client.MongoCollection;
+import com.mongodb.client.MongoDatabase;
+import com.sun.org.apache.bcel.internal.generic.DCMPG;
+import org.bson.Document;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class TestMongodb {
+
+    public static void main(String[] args) {
+
+        try {
+            //1.链接到mongodb服务
+            MongoClient mongoClient = new MongoClient("113.209.26.156", 27017);
+            //2.链接导数据库
+            MongoDatabase mongoDatabase = mongoClient.getDatabase("zcy_mongodb");
+
+            MongoCollection<Document> collection = mongoDatabase.getCollection("zcy_mongodb");
+
+            //3.插入文档
+            Document document = new Document("title","MongoDbDocument_java").append("description","database").append("likes",100).append("by","fly");
+            Document document2 = new Document("title","MongoDbDocument_java2").append("description","database2").append("likes",1002).append("by","fly2");
+            List<Document> list = new ArrayList<Document>();
+            list.add(document);
+            list.add(document2);
+
+            collection.insertMany(list);
+
+            System.out.println("链接数据库成功");
+        }catch (Exception e){
+            e.printStackTrace();
+        }finally {
+
+        }
+    }
+}
