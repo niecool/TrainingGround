f8e1de0a0e4dcf0759a920fee86ea492f1c0c55a
zhaochengye <zhaochengye@jd.com>
2019-02-18 15:23:51 +0800

    重置master空工程2

--- cms/src/main/java/com/jd/cms/test/impl/TestImpl.java
+++ cms/src/main/java/com/jd/cms/test/impl/TestImpl.java
@@ -1,17 +1,6 @@
 package com.jd.cms.test.impl;
 
 import com.jd.cms.test.Test;
-import com.jd.cms.test.hdfs.HDFSUtils;
-import org.apache.hadoop.fs.FileSystem;
-import org.apache.hadoop.fs.Path;
-
-import java.io.BufferedReader;
-import java.io.File;
-import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.Iterator;
-import java.util.List;
-import java.util.Set;
 
 /**
  * @author zhaochengye
